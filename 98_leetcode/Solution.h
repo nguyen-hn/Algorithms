50464c87118ba1442a497f56877d6f520ae37723
nguyen.hoang <nguyen273@gmail.com>
2024-06-06 21:44:24 +0700

    daily leetcode 846

---
+++ 98_leetcode/Solution.h
@@ -0,0 +1,16 @@
+#ifndef SOLUTION_H
+#define SOLUTION_H
+
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    // 846. Hand of Straights
+    bool isNStraightHand(vector<int>& hand, int groupSize);
+};
+
+#endif // SOLUTION_H
