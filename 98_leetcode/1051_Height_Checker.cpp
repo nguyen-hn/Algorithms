30693f676e8b702737deae3b112b0878003ce05b
nguyen.hoang <nguyen273@gmail.com>
2024-06-10 22:10:12 +0700

    daily problem 1051

---
+++ 98_leetcode/1051_Height_Checker.cpp
@@ -0,0 +1,11 @@
+#include "Solution.h"
+
+int Solution::heightChecker(vector<int>& heights) {
+    vector<int> expected = heights;
+    sort(expected.begin(), expected.end());
+    int cnt = 0;
+    for(int i = 0; i < heights.size(); i++) {
+        if (expected[i] != heights[i]) cnt++;
+    }
+    return cnt;
+}
